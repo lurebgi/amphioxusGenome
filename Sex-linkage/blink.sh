4fb1653bd0c8b46ea21f603fbb243b8a412e8474
Luohao Xu <luohaox@gmail.com>
2019-03-24 13:05:25 +0100

    Update blink.sh

--- Sex-linkage/blink.sh
+++ Sex-linkage/blink.sh
@@ -13,6 +13,6 @@ module load vcftools
 vcftools --gzvcf bb.snp.final.vcf.gz  --max-missing 0.9 --maf 0.05 --min-meanDP 5 --recode --recode-INFO-all --out  $TMPDIR/bb.snp.final.filter.vcf
 mv $TMPDIR/bb.snp.final.filter.vcf* .
 
-/apps/java/1.8u152/bin/java -Xmx158G -jar /scratch/luohao/software/beagle.28Sep18.793.jar  gt=bb.snp.final.filter.vcf.recode.vcf out=bb.snp.final.filter.vcf.recode.IM.vcf nthreads=12
-sed -i  -e 's/Chr//' -e 's#0|#0/#g' -e 's#1|#1/#g' bb.snp.final.filter.vcf.recode.IM.vcf.vcf
-/scratch/luohao/software/BLINK/blink_linux --gwas --vcf --file bb.snp.final.filter.vcf.recode.IM.vcf --out bb.snp.final.IM.out
+/apps/java/1.8u152/bin/java -Xmx158G -jar /scratch/luohao/software/beagle.28Sep18.793.jar  gt=bb.snp.final.filter.vcf.recode.vcf out=bb.snp.final.filter.vcf.recode.IM nthreads=12
+sed -i  -e 's/Chr//' -e 's#0|#0/#g' -e 's#1|#1/#g' bb.snp.final.filter.vcf.recode.IM.vcf
+/scratch/luohao/software/BLINK/blink_linux --gwas --vcf --file bb.snp.final.filter.vcf.recode.IM --out bb.snp.final.IM.out
